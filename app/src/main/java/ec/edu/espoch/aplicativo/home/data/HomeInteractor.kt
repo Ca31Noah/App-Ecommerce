cc7c5dda7a9939c6f395322018de99276f91f6b9
Ca31Noah <carlos.noa2001@gmail.com>
2024-07-15 07:34:22 -0500

    Home

--- app/src/main/java/ec/edu/espoch/aplicativo/home/data/HomeInteractor.kt
+++ app/src/main/java/ec/edu/espoch/aplicativo/home/data/HomeInteractor.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeInteractor {
 
-    private val BASE_URL = "http://192.168.2.111/android_mysql/"
+    private val BASE_URL = "http://10.0.2.2/android_mysql/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
